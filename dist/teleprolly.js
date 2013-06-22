c06891418dafacf215f5b64d59e035aa35ab95df
Marcos Caceres <marcosscaceres@gmail.com>
2013-06-22 22:10:02 +0100

    regenerated file

--- dist/teleprolly.js
+++ dist/teleprolly.js
@@ -1,45 +1,6 @@
-/*! teleprolly - v0.1.0 - 2013-06-11
+/*! teleprolly - v0.1.0 - 2013-06-22
 * https://github.com/marcoscaceres/teleprolly
 * Copyright (c) 2013 Marcos Caceres; Licensed MIT */
-(function(){
-    "use strict";
-    require.config({
-        paths: {
-            'deps': '../deps',
-            'DOM': '../deps/DOM',
-            'WebIDL': '../webidl/lib/WebIDL',
-            'deps/webidlParser': '../webidl/deps/webidlParser',
-            'DOM/Futures' : '../deps/DOM/Futures',
-            'DOM/EventHandler': '../deps/DOM/EventHandler',
-            'DOM/EventTarget' : '../deps/DOM/EventTarget',
-
-            //create alias to plugins (not needed if plugins are on the baseUrl)
-            async: '../deps/require-plugins/async',
-            goog: '../deps/require-plugins/goog',
-            json: '../deps/require-plugins/json',
-            noext: '../deps/require-plugins/noext',
-            propertyParser: '../deps/sync/propertyParser'
-        }
-    });
-
-    // convert Futures into an AMD module
-    define('Futures', ['DOM/Futures'],
-    function() {
-        // return the gmaps namespace for brevity
-        return window.Future;
-    });
-
-
-    // convert Google Maps into an AMD module
-    define('Geocoder', ['async!http://maps.google.com/maps/api/js?v=3&sensor=false'],
-    function() {
-        // return the gmaps namespace for brevity
-        return new window.google.maps.Geocoder();
-    });
-
-    require(['interfaces/Navigator']);
-}());
-
 define(['WebIDL', 'interfaces/TelephonyManager'], function() {
 	'use strict';
 	var	WebIDL = require('WebIDL'),
